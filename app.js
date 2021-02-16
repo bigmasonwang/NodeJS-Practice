500551f357ce32aff618d7faba84eaec27d978b8
bigmasonwang <bigmasonwang@gmail.com>
2021-02-16 12:53:40 +1030

    creat node server

---
+++ app.js
@@ -0,0 +1,9 @@
+// ES5
+// const http = require('http');
+// function requestListener(req, res) {}
+// http.createServer(requestListener);
+
+// ES6
+import { createServer } from 'http';
+
+createServer((req, res) => {});
