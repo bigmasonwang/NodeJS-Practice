d7868b0973ea5ef3af8460800e75f6cddcf155ca
bigmasonwang <bigmasonwang@gmail.com>
2021-02-16 12:59:54 +1030

    server

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 // ES5
-// const http = require('http');
+const http = require('http');
 // function requestListener(req, res) {}
 // http.createServer(requestListener);
 
 // ES6
-import { createServer } from 'http';
+// import { createServer } from 'http';
 
-createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  console.log(req);
+});
+
+server.listen(3000);
