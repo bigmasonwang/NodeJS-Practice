20bb6fd2956d72aaeda490b098b3e839122580d6
bigmasonwang <bigmasonwang@gmail.com>
2021-02-16 13:15:29 +1030

    send response

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const http = require('http');
 // import { createServer } from 'http';
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  // console.log(req.url, req.method, req.headers);
+  // process.exit();
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>First Page</title></head>');
+  res.write('<body><h1>Hello World</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 
 server.listen(3000);
