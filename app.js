afc90f547b1ce53d7bea7e1146677577df819f7f
Mason <bigmasonwang@gmail.com>
2021-02-17 22:39:56 +1030

    parse incoming request

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const http = require('http');
 const express = require('express');
-const { StringDecoder } = require('string_decoder');
-
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('in the middleware');
-  next();
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>'
+  );
 });
 
-app.use((req, res, next) => {
-  console.log('in the another middleware');
+app.use('/product', (req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
   res.send('<h1>Hello from express</h1>');
-  next();
 });
 
 app.listen(3000);
